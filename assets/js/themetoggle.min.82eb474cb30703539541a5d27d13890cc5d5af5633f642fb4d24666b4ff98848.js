399d2714298c4b0c6d29c361c365197ec590a5f1
wilspi <thewilspi@gmail.com>
2023-10-22 01:31:26 +0530

    update

---
+++ assets/js/themetoggle.min.82eb474cb30703539541a5d27d13890cc5d5af5633f642fb4d24666b4ff98848.js
@@ -0,0 +1,1 @@
+document.getElementById("theme-toggle").addEventListener("click",()=>{document.body.className.includes("dark")?(document.body.classList.remove("dark"),localStorage.setItem("pref-theme","light")):(document.body.classList.add("dark"),localStorage.setItem("pref-theme","dark"))})
